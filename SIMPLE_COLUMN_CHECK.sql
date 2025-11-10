067d8cfd31365632f71b232974fb726140a45264
Phuc Hai Le <phuchai.le@gmail.com>
2025-11-10 17:18:11 +0700

    cleanup: Remove 35+ junk files (MD, TXT, SQL) - 85% reduction in documentation clutter

--- SIMPLE_COLUMN_CHECK.sql
+++
@@ -1,8 +0,0 @@
--- Simple query to check column names in analysis_variables
--- Copy and paste this into Supabase SQL Editor
-
-SELECT column_name
-FROM information_schema.columns
-WHERE table_schema = 'public'
-  AND table_name = 'analysis_variables'
-ORDER BY ordinal_position;
